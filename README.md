93678ef6814aa9d0295a7cefd69154999207536a
Dave Pearson <davep@davep.org>
2023-12-25 14:05:19 +0000

    :tada: Bare bones framework in which to build the application

---
+++ README.md
@@ -0,0 +1,5 @@
+# Coming Soon(ish)
+
+Coming 2024, all being well.
+
+[//]: # (README.md ends here)
---
+++ oshit/__main__.py
@@ -0,0 +1,13 @@
+"""The main entry point for the application."""
+
+##############################################################################
+def run() -> None:
+    """Run the application."""
+    print("This will do something useful in the next release.")
+
+
+##############################################################################
+if __name__ == "__main__":
+    run()
+
+### __main__.py ends here
